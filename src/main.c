ec089e1dbdbfecccbb501e8885a61798bdc8cf3c
LukasPio <lucaspio.galvao@gmail.com>
2026-09-13 17:52:22 -0300

    Refactor game logic to include font setup, input handling for restart, and game result display

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #define SDL_ERROR 1
 
@@ -24,8 +26,16 @@
 #define BACKGROUND_COLOR 0, 0, 0, SDL_ALPHA_OPAQUE
 #define SPRITE_COLOR 255, 165, 0, SDL_ALPHA_OPAQUE
 
+enum Result
+{
+    VICTORY,
+    LOSE
+};
+
 void on_sdl_error(void);
 void setup_graphics(void);
+void setup_fonts(void);
+void reset_variables(void);
 void setup_paddles(void);
 void setup_ball(void);
 void start_game(void);
@@ -34,11 +44,16 @@ void handle_input(void);
 void move_ball(void);
 void move_computer_paddle(void);
 void check_collision(void);
+void show_game_result(enum Result result);
 
 SDL_Window *w = NULL;
 SDL_Renderer *r = NULL;
+TTF_Font *f = NULL;
 
 int running = 0;
+int wait_input = 0;
+int restart = 0;
+int quit = 0;
 int computer_delay = COMPUTER_PADDLE_DELAY;
 int ball_speed = BALL_INITIAL_SPEED;
 int increase_ball_speed_interval = INCREASE_BALL_SPEED_INTERVAL;
@@ -51,9 +66,13 @@ int ball_direction_x, ball_direction_y;
 
 int main()
 {
-    srand(time(NULL));
-
     setup_graphics();
+    setup_fonts();
+
+restart:
+    srand(time(NULL));
+    reset_variables();
+
     setup_paddles();
     setup_ball();
 
@@ -68,6 +87,17 @@ int main()
         check_collision();
     }
 
+    while (wait_input)
+    {
+        handle_input();
+        if (restart)
+        {
+            goto restart;
+        }
+        if (quit)
+            break;
+    }
+
     SDL_Quit();
 }
 
@@ -79,6 +109,9 @@ void on_sdl_error(void)
 
 void setup_graphics(void)
 {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0 || TTF_Init() == -1)
+        on_sdl_error();
+
     w = SDL_CreateWindow("PONG", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (w == NULL)
         on_sdl_error();
@@ -96,6 +129,20 @@ void setup_graphics(void)
     SDL_SetRenderDrawColor(r, BACKGROUND_COLOR);
     SDL_RenderClear(r);
     SDL_RenderPresent(r);
+}
+
+void setup_fonts(void)
+{
+    f = TTF_OpenFont("./font/arial_font.ttf", 256);
+    TTF_SetFontStyle(f, TTF_STYLE_BOLD);
+}
+
+void reset_variables(void)
+{
+    running = 0;
+    quit = 0;
+    restart = 0;
+    ball_speed = BALL_INITIAL_SPEED;
 }
 
 void setup_paddles(void)
@@ -153,6 +200,7 @@ void handle_input(void)
             {
             case SDL_SCANCODE_MINUS:
                 running = 0;
+                quit = 1;
                 break;
             case SDL_SCANCODE_W:
                 player.y -= PADDLE_SPEED;
@@ -173,6 +221,10 @@ void handle_input(void)
                 player.y += PADDLE_SPEED;
                 if (player.y > SCREEN_HEIGHT - SCREEN_VERTICAL_GAP - PADDLE_HEIGHT)
                     player.y = SCREEN_HEIGHT - SCREEN_VERTICAL_GAP - PADDLE_HEIGHT;
+                break;
+            case SDL_SCANCODE_R:
+                if (!running)
+                    restart = 1;
                 break;
             }
             break;
@@ -203,7 +255,7 @@ void move_computer_paddle(void)
     computer_delay--;
     if (computer_delay > 0)
         return;
-    if (computer.y + PADDLE_HEIGHT / 2 > ball.y  + BALL_HEIGHT / 2)
+    if (computer.y + PADDLE_HEIGHT / 2 > ball.y + BALL_HEIGHT / 2)
         computer.y -= PADDLE_SPEED;
     else
         computer.y += PADDLE_SPEED;
@@ -235,4 +287,48 @@ void check_collision(void)
 
     if (ball.y >= SCREEN_HEIGHT - SCREEN_VERTICAL_GAP - BALL_HEIGHT)
         ball_direction_y *= -1;
+
+    if (ball.x <= 0)
+        show_game_result(LOSE);
+
+    if (ball.x >= SCREEN_WIDTH)
+        show_game_result(VICTORY);
+}
+
+void show_game_result(enum Result result)
+{
+    running = 0;
+    wait_input = 1;
+
+    char *result_message = result == VICTORY ? "You won! Congratulations :)" : "You lose... Try again!";
+    char *restart_message = "Press R to restart the game.";
+
+    SDL_Color text_color = {SPRITE_COLOR};
+
+    SDL_Surface *result_surface = TTF_RenderText_Blended(f, result_message, text_color);
+    SDL_Texture *result_texture = SDL_CreateTextureFromSurface(r, result_surface);
+    SDL_FreeSurface(result_surface);
+
+    SDL_Surface *restart_surface = TTF_RenderText_Blended(f, restart_message, text_color);
+    SDL_Texture *restart_texture = SDL_CreateTextureFromSurface(r, restart_surface);
+    SDL_FreeSurface(restart_surface);
+
+    SDL_Rect result_dst;
+    result_dst.h = SCREEN_HEIGHT / 3;
+    result_dst.w = SCREEN_WIDTH / 2;
+    result_dst.x = (SCREEN_WIDTH - result_dst.w) / 2;
+    result_dst.y = (SCREEN_HEIGHT - result_dst.h) / 2;
+
+    SDL_Rect restart_dst;
+    restart_dst.h = SCREEN_HEIGHT / 5;
+    restart_dst.w = SCREEN_WIDTH / 3;
+    restart_dst.x = result_dst.x + (result_dst.w - restart_dst.w) / 2;
+    restart_dst.y = result_dst.y + restart_dst.h + 50;
+
+    SDL_SetRenderDrawColor(r, BACKGROUND_COLOR);
+    SDL_RenderClear(r);
+
+    SDL_RenderCopy(r, result_texture, NULL, &result_dst);
+    SDL_RenderCopy(r, restart_texture, NULL, &restart_dst);
+    SDL_RenderPresent(r);
 }
